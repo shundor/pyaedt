cda392de2f692d468238a7afc45f8a6c2b93b25f
Abir Majumdar <83433840+abirismyname@users.noreply.github.com>
2022-08-31 17:27:40 +0000

    Adding new setup code again

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 log = logging.getLogger()
 
-azure_devops_pat = "cfdgksmyyogbj6b6k7tvu6wfgotk453xxadybrrs22g6qekcaw3y"
+azure_devops_pat = "67csyuknm2k4azb2q3ktjb7dwnafo2xj5bwpv7vt7ei2l5a7wwf"
 from _setup_common import (
     name,
     version,
